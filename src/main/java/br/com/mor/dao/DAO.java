6a30c3ab5c27e1fb5f7f7a980725e25e4a08d68e
LUAN <LUAN@192.168.25.89>
2016-08-09 20:38:55 -0300

    Daos e Generics

---
+++ src/main/java/br/com/mor/dao/DAO.java
@@ -0,0 +1,24 @@
+package br.com.mor.dao;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @author lucas.oliveira
+ *
+ * @param <T> - Entidade Persistente
+ * @param <ID> - Chave Primária
+ */
+public interface DAO <T, ID extends Serializable> {
+
+	T salvar(T entidade);
+	
+	void deletar(T entidade);
+	
+	long contar();
+
+	List<T> buscar();
+	
+	List<T> buscar(int offset, int max);
+	
+}
---
+++ src/main/java/br/com/mor/dao/GenericDAO.java
@@ -0,0 +1,76 @@
+package br.com.mor.dao;
+
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+/**
+ * 
+ * @author lucas.oliveira
+ *
+ * @param <T> Tipo do dominio associado ao DAO
+ */
+public class GenericDAO<T> implements DAO<T,Long> {
+	
+	private SessionFactory sessionFactory = SessionFactoryHolder
+												.getSessionFactory();
+	
+	private Class<T> dominio;
+	
+	public GenericDAO(Class<T> dominio){	
+		this.dominio = dominio;
+	}
+	
+	public Session getCurrentSession(){
+		return sessionFactory.getCurrentSession();
+	}
+	
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+	
+	public Class<T> getDominio(){
+		return dominio;
+	}
+	
+	
+	public T salvar(T entidade) {
+		getCurrentSession().saveOrUpdate(entidade);
+		return entidade;
+	}
+
+	
+	public void deletar(T entidade) {
+		getCurrentSession().delete(entidade);
+	}
+
+	
+	public List<T> buscar() { 
+		CriteriaBuilder builder = getCurrentSession().getCriteriaBuilder();			
+		CriteriaQuery<T> criteria = builder.createQuery(dominio);			
+		criteria.from(dominio);
+		return getCurrentSession().createQuery(criteria).getResultList();	
+	}
+
+	
+	public long contar() {
+		CriteriaBuilder builder = getCurrentSession().getCriteriaBuilder();			
+		CriteriaQuery<Long> criteria = builder.createQuery(Long.class);			
+		criteria.select(builder.count(criteria.from(dominio)));
+		return getCurrentSession().createQuery(criteria).getSingleResult();
+	}
+
+	
+	public List<T> buscar(int offset, int max) { 
+		CriteriaBuilder builder = getCurrentSession().getCriteriaBuilder();			
+		CriteriaQuery<T> criteria = builder.createQuery(dominio);			
+		criteria.from(dominio);
+		return getCurrentSession().createQuery(criteria)
+				.setFirstResult(offset).setMaxResults(max).getResultList();
+	}
+	
+}
